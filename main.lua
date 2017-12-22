1636f46287b9c3d40c2a523e37e165d706dce806
Alpiona <alpiona@hotmail.com>
2017-12-22 17:33:59 -0200

    Primeiro commit

---
+++ main.lua
@@ -0,0 +1,5 @@
+tabelaAlunos = Alunos:criaTabela("alunos.csv")
+tabelaAvaliacoes = Avaliacoes:criaTabela("avaliacoes.csv")
+tabelaCursos = Cursos:criaTabela("cursos.csv")
+tabelaDisciplinas = Disciplinas:criaTabela("disciplinas.csv")
+tabelaNotas = Notas:criaTabela("notas.csv")
